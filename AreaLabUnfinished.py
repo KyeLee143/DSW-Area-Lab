c573d8788d6e569a759c1823c7eeb3a3e3d2e7ac
KyeLee143 <keilipalmares8455@gmail.com>
2019-09-30 10:01:59 -0700

    circleArea and TriArea

--- AreaLabUnfinished.py
+++ AreaLabUnfinished.py
@@ -1,26 +1,32 @@
-import math 
+import math #math library has pre-written 
 import unittest
 
-def circleArea(radius): 
-    return 1
+def circleArea(radius):
+    """Return the area of a circle"""
+    return math.pi * radius * radius
 
 def rectArea(base, height):
-    return 1
+    """Return the area of a rect"""
+    return base * height 
 
 def trapArea(base1, base2, height):
-    return 1
+    """Return the area of a trapezoid"""
+    return base1 + base2 * 1/2 * height 
 
 def triArea(base,height):
-    return 1
+    """Return the area of a tri"""
+    return base * height /2
     
 	
 class MyTest(unittest.TestCase):
     def testCircleArea(self):
+        self.assertEqual(circleArea(5), 25*math.pi)
+    #def testRectArea(self):
     
-    def testRectArea(self):
-    
-    def testTrapArea(self):
+    #def testTrapArea(self):
     
     def testTriArea(self):
+    self.assertEqual(triArea(3,3), 4.
+    self.assertAlmostEqual(triArea(4.
     
     
